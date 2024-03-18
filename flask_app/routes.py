aa193b2ba6bf7d8b5c8bfdd3e22ae09145c7aee1
aprizalabyan <abi@jkt1.ebdesk.com>
2024-03-18 14:47:48 +0700

    feat: handle update & delete data

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, make_response, jsonify, render_template
 from .models import user_collection
 from datetime import datetime
+from bson import ObjectId
 
 bp = Blueprint("routes", __name__)
 
@@ -11,7 +12,7 @@ def index():
     # return render_template("index.html")
 
 
-@bp.route("/user", methods=["GET", "POST"])
+@bp.route("/user", methods=["GET", "POST", "PUT", "DELETE"])
 def user():
     data = []
     message = ""
@@ -24,8 +25,10 @@ def user():
                 u["updatedAt"] = u["updatedAt"] if "updatedAt" in u else ""
             data = list_users
             make_response().status = 200
+            message = "Get data success"
         except:
             make_response().status = 500
+            message = "Get data failed"
     elif request.method == "POST":
         try:
             payload = request.json
@@ -36,11 +39,42 @@ def user():
                     "createdAt": datetime.now(),
                     "updatedAt": datetime.now(),
                 }
-                print("postt", new_user)
                 user_collection.insert_one(new_user)
                 make_response().status = 200
+                message = "Data added successfully"
         except:
             make_response().status = 500
+            message = "Failed to add"
+    elif request.method == "PUT":
+        try:
+            payload = request.json
+            user_id = request.args.get("id")
+            if payload and user_id:
+                user_collection.update_one(
+                    {"_id": ObjectId(user_id)},
+                    {
+                        "$set": {
+                            "name": payload["name"],
+                            "email": payload["email"],
+                            "updatedAt": datetime.now(),
+                        }
+                    },
+                )
+                make_response().status = 200
+                message = "Data updated successfully"
+        except:
+            make_response().status = 500
+            message = "Failed to update"
+    elif request.method == "DELETE":
+        try:
+            user_id = request.args.get("id")
+            if user_id:
+                user_collection.delete_one({"_id": ObjectId(user_id)})
+                make_response().status = 200
+                message = "Data deleted successfully"
+        except:
+            make_response().status = 500
+            message = "Failed to delete"
 
     response = jsonify(
         {"status": make_response().status_code, "message": message, "data": data}
